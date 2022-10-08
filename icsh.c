9c74e76e237233ac53ac204b4fce55ff1e107833
gameyp <yossatorn.pht@student.mahidol.edu>
2022-10-08 22:13:25 +0700

    Add command

--- icsh.c
+++ icsh.c
@@ -4,14 +4,41 @@
  */
 
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 #define MAX_CMD_BUFFER 255
+
+void command(char *);
+void check_rest(char *);
 
 int main() {
     char buffer[MAX_CMD_BUFFER];
     while (1) {
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
-        printf("you said: %s\n", buffer);
+        command(buffer);
+        // printf("%c", buffer[1]);
+        // printf("you said: %s\n", buffer);
     }
+}
+
+void command(char *buffer) {
+    if (strstr(buffer, "echo")){
+        char *res;
+        for (res = buffer ; *res && *res != ' ' ; res++)
+            ;
+        if (*res) res++;
+        printf("%s", res);
+    } 
+    else if (strstr(buffer, "!!")){
+        printf("Good");
+    } 
+    else if (strstr(buffer, "exit")) {
+        printf("Bye");
+    }
+}
+
+void check_rest(char *) {
+    
 }
