007ce548a4422a62a585a0be71e3f1e4eda0d6f1
gameyp <yossatorn.pht@student.mahidol.edu>
2022-10-10 00:28:13 +0700

    Milestone 1 finished
    add history
    add !!
    exit()
    bad command

--- icsh.c
+++ icsh.c
@@ -3,35 +3,39 @@
  * StudentID: 6280942
  */
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
 
 #define MAX_CMD_BUFFER 255
 
 char *res;
+char wow[];
 void command(char *);
+void my_history(char *);
 
 int main() {
-    char buffer[MAX_CMD_BUFFER];
-    while (1) {
-        printf("icsh $ ");
+	char buffer[MAX_CMD_BUFFER];
+	while(1) {
+		printf("icsh $ ");
         fgets(buffer, 255, stdin);
         command(buffer);
-        // printf("%c", buffer[1]);
-        // printf("you said: %s\n", buffer);
-    }
+	}
 }
 
 void command(char *buffer) {
     if (strstr(buffer, "echo")){
+		my_history(buffer);
         for (res = buffer ; *res && *res != ' ' ; res++)
             ;
         if (*res) res++;
         printf("%s", res);
     } 
     else if (strstr(buffer, "!!")){
-        printf("Good");
+		strcpy(buffer, wow);
+		printf("%s", buffer);
+		command(buffer);
     } 
     else if (strstr(buffer, "exit")) {
         for (res = buffer ; *res && *res != ' ' ; res++)
@@ -43,6 +47,11 @@ void command(char *buffer) {
         } else {
             printf("bad command\n");
         }
+    } else {
+		printf("bad command\n");
+	}
+}
 
-    }
+void my_history(char *buffer){
+	strcpy(wow, buffer);
 }
